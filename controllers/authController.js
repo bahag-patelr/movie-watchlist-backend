d18613c73afb257579f2a0e1b88725a23dd34539
Maximilian Reed <maximilian.reed@bahag.com>
2026-03-31 12:05:14 +0200

    initialised db settings

---
+++ controllers/authController.js
@@ -0,0 +1,3 @@
+export const registerNewUser = async (req, res) => {  }
+export const loginUser = async (req, res) => {  }
+export const getUser = async (req, res) => {  }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config"
+import sequelize from "./db/db.js";
 
 const app = express();
 const PORT  = process.env.PORT || 3000;
